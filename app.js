bb6c6eeb5b5e2198a932b3cb19e4672f79bb7a3c
Sam Williams <willsam100@Sams-MacBook-Pro.local>
2015-06-18 21:49:26 +1200

    The real question

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.get('/question', function(req, res) {
 
   console.log('Question was asked');
   // res.send('would you like to go on a date with me?');
-  res.send('what is your favourite movie?');
+  res.send('would you like to go on a data with me?');
 });
 
 
